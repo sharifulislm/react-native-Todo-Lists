1b35642ec920c9d4234b833a9d0b77334729e831
sharifulislm <shariful.islam.coder@gmail.com>
2022-10-09 13:29:38 +0300

    basic data color and home page is doen

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Colors from './Colors';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <View style={{flexDirection: "row"}}>
+        <View style={styles.divider}/>
+        <Text style={styles.title}> Todo 
+       <Text style={{fontWeight:"300", color:Colors.blue}}>Lists</Text>
+        </Text>
+        <View style={styles.divider}/>
+        
+      </View>
+      <View style={{marginVertical: 48}}>
+      <TouchableOpacity style={styles.addList}>
+      <AntDesign name="plus" size={24} color="black" />
+      </TouchableOpacity>
+      <Text style={styles.add}> Add List</Text>
+      </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  divider: {
+    backgroundColor:Colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center"
+  },
+  title: {
+    fontSize:38,
+    fontWeight:"800",
+    color: Colors.black,
+    paddingHorizontal: 64
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: Colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  add: {
+    color: Colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    margin: 8
+
+  }
 });
---
+++ Colors.js
@@ -0,0 +1,6 @@
+export default Colors = {
+    black: "#2D3436",
+    blue: "#24AGD9",
+    lightBlue: "#A7CBD9",
+    white: "#FFFFF"
+}
