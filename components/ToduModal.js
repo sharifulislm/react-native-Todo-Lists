2f2ea9fc709978c3292ad1ca59690ca3001f91a4
sharifulislm <shariful.islam.coder@gmail.com>
2022-11-26 15:56:59 +0300

    useing flatlist from react native and showing filtering data with title

--- components/ToduModal.js
+++ components/ToduModal.js
@@ -1,14 +1,27 @@
 import { AntDesign } from '@expo/vector-icons';
 import React from 'react';
-import { SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
+import { FlatList, KeyboardAvoidingView, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import color from '../Colors';
 
 export default class ToduModal extends React.Component {
     state={
         name: this.props.list.name,
         color: this.props.list.color,
         todos: this.props.list.todos
+    };
+
+    renderTodo = todo => {
+        return (
+            <View>
+                <Text>{todo.title}</Text>
+            </View>
+        )
     }
+
   render() {
+   const taskCount = this.state.todos.length
+   const completedCount = this.state.todos.filter(todo => todo.completed).length
+
     return (
       <SafeAreaView style={styles.container}>
         <TouchableOpacity 
@@ -17,6 +30,30 @@ export default class ToduModal extends React.Component {
         >
         <AntDesign name="close" size={24} color="black" />
         </TouchableOpacity>
+        <View style={[styles.section, styles.header,{borderBottomColor: this.state.color}]}>
+            <View >
+                <Text style={styles.title}>{this.state.name}</Text>
+                <Text style={styles.taskCount}>
+               {completedCount} of {taskCount} tasks
+                </Text>
+
+            </View>
+        </View>
+        <View style={[styles.section,{flex: 3}]}>
+          <FlatList data={this.state.todos} renderItem={ ({item}) => this.renderTodo(item)}
+          keyExtractor={item => item.title}
+          contentContainerStyle={{paddingHorizontal: 32,paddingVertical: 64}}
+          showsVerticalScrollIndicator={false}
+
+          />
+        </View>
+        <KeyboardAvoidingView style={[styles.section, styles.footer]} behavior="padding">
+            <TextInput style={[styles.input, {borderColor: this.state.color}]} />
+            <TouchableOpacity style={[styles.addTodu, {backgroundColor: this.state.color}]}>
+            <AntDesign name="plus" size={24} color="black" />
+            </TouchableOpacity>
+
+        </KeyboardAvoidingView>
 
       </SafeAreaView>
     )
@@ -28,5 +65,46 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItem: "center"
+    },
+    section: {
+        flex: 1,
+        alignSelf: "stretch"
+    },
+    header: {
+        justifyContent: "flex-end",
+        marginLeft: 64,
+        borderBottomWidth:3
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "800",
+        color: color.black
+    },
+    taskCount:{
+        marginTop: 4,
+        marginBottom:16,
+        color: color.gray,
+        fontWeight: "600"
+    },
+    footer: {
+        paddingHorizontal: 32,
+        flexDirection: "row",
+        alignItems: "center"
+
+    },
+    input: {
+        flex: 1,
+        height: 48,
+        borderWidth:StyleSheet.hairlineWidth,
+        borderRadius: 6,
+        marginRight: 8,
+        paddingHorizontal:8
+    },
+    addTodu: {
+        borderRadius: 4,
+        padding: 16,
+        alignItems: "center",
+        justifyContent: "center"
     }
+
 });
